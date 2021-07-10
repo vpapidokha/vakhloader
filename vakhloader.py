ddc21541bad3d36dbbcca45168b6fea07237b99e
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2021-07-11 00:11:21 +0200

    Added uploading of the generated audio file to remote server

--- vakhloader.py
+++ vakhloader.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 from requests.api import get
+from paramiko import SSHClient, RSAKey, AutoAddPolicy
+from scp import SCPClient
 import youtube_dl
 import sys
 import logging
@@ -33,7 +35,7 @@ def getAudioFromYoutubeVideo(videoTitle, videoURL, storagePath):
     episodeNumber = getEpisodeNumber(videoTitle)
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': f"{storagePath}/vakhmurky_{episodeNumber}.%(ext)s",
+        'outtmpl': f"{storagePath}/{episodeNumber}.mp3",
         'retries': 3,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -45,14 +47,43 @@ def getAudioFromYoutubeVideo(videoTitle, videoURL, storagePath):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([videoURL])
 
+    return episodeNumber
+
+def progressForScp(filename, size, sent):
+    sys.stdout.write("%s's progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
+
+def sendFileToTargetServer(targetServer, pathToKeyFile, sourcePath, targetPath, fileName):
+    sshKey = RSAKey.from_private_key_file(pathToKeyFile)
+    sshConnection = SSHClient()
+    sshConnection.set_missing_host_key_policy(AutoAddPolicy())
+
+    logging.info(f"Connecting to the {targetServer} server...")
+    sshConnection.connect(hostname = targetServer, username = "root", pkey = sshKey)
+    logging.info(f"Connected")
+
+    scp = SCPClient(sshConnection.get_transport(), progress=progressForScp)
+    logging.info(f"Sending {fileName} to the {targetServer} server...")
+    scp.put(f"{sourcePath}/{fileName}", f"{targetPath}/{fileName}")
+    logging.info(f"Sent")
+
+    scp.close()
 
 def main():
     if len(sys.argv) < 2:
         raise Exception('Missing command line parameters')
-    
+
+    apiKey = sys.argv[1]
+    channelId = sys.argv[2]
+    localStoragePath = sys.argv[3]
+    targetServer = sys.argv[4]
+    pathToSshKey = sys.argv[5]
+    targetStoragePath = sys.argv[6]
+
     logging.basicConfig(level = logging.INFO)
-    lastVideoInfo = getLastVideoInfo(apiKey = sys.argv[1], channelId = sys.argv[2])
-    getAudioFromYoutubeVideo(lastVideoInfo['title'], f"https://www.youtube.com/watch?v={lastVideoInfo['id']}", storagePath = sys.argv[3])
+
+    lastVideoInfo = getLastVideoInfo(apiKey, channelId)
+    episodeNumber = getAudioFromYoutubeVideo(lastVideoInfo['title'], f"https://www.youtube.com/watch?v={lastVideoInfo['id']}", localStoragePath)
+    sendFileToTargetServer(targetServer, pathToSshKey, localStoragePath, targetStoragePath, f"{episodeNumber}.mp3")
 
 if __name__ == "__main__":
     main()
